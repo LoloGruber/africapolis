7e65839cb15332bc5aca3746a5b9b79771e36aef
Lorenz Gruber <lorenz.gruber@uni-wuerzburg.de>
2026-07-20 17:39:44 +0200

    adding graph construction customization

--- src/GraphConstruction.cpp
+++ src/GraphConstruction.cpp
@@ -1,4 +1,5 @@
 #include <CLI/CLI.hpp>
+#include <magic_enum.hpp>
 #include <nlohmann/json.hpp> 
 #include <fishnet/Fishnet.hpp>
 #include <fishnet/Task.hpp>
@@ -9,6 +10,8 @@
 #include <fishnet/SettlementShape.hpp>
 #include <fishnet/PolygonNeighbours.hpp>
 #include "BinarySettlementGraphAdjacency.hpp"
+#include "fishnet/FunctionalConcepts.hpp"
+#include "fishnet/ShapeGeometry.hpp"
 
 using json = nlohmann::json;
 
@@ -40,16 +43,27 @@ public:
     }
 };
 
+enum class GraphConstructionMode {
+    BUFFER_SWEEP,
+    DELAUNAY
+};
+
 struct GraphConstructionConfig {
-    constexpr static const char * BUFFER_DISTANCE_KEY = "buffer-distance-meters";
+    constexpr static const char * GRAPH_CONSTRUCTION_KEY = "graph-construction";
+    constexpr static const char * GRAPH_CONSTRUCTION_MODE = "mode";
+    constexpr static const char * GRAPH_CONSTRUCTION_ARGS = "args";
+    constexpr static const char * DISTANCE_THRESHOLD_KEY = "distance-threshold";
     constexpr static const char * MAX_NEIGHBORS_KEY = "max-neighbors-per-node";
 
-    double bufferDistanceMeters;
-    size_t maxNeighborsPerNode;
+    GraphConstructionMode mode;
+    json args;
+    double distanceThreshold;
 
     GraphConstructionConfig(const json & config) {
-        bufferDistanceMeters = config.at(BUFFER_DISTANCE_KEY).get<double>();
-        maxNeighborsPerNode = config.at(MAX_NEIGHBORS_KEY).get<size_t>();
+        auto graphConstructionConfig = config.at(GRAPH_CONSTRUCTION_KEY);
+        this->mode = magic_enum::enum_cast<GraphConstructionMode>(graphConstructionConfig.at(GRAPH_CONSTRUCTION_MODE).get<std::string>()).value();
+        this->args = graphConstructionConfig.at(GRAPH_CONSTRUCTION_ARGS);
+        this->distanceThreshold = this->args.at(DISTANCE_THRESHOLD_KEY).get<double>();
     }
 };
 
@@ -61,6 +75,36 @@ private:
     DistanceFunction distanceFunction;
     std::filesystem::path graphBinaryOutputPath;
     std::unordered_map<FileReference, std::filesystem::path> fileRefMap;
+
+
+    std::vector<std::pair<SettlementShape<S>,SettlementShape<S>>> compute_neighbours() {
+        const auto distancePredicate = DistanceBiPredicate(distanceFunction,this->config.distanceThreshold);
+        switch (config.mode) {
+            case GraphConstructionMode::DELAUNAY:
+                return fishnet::geometry::PolygonNeighbours::delaunay(this->settlements,distancePredicate);
+            case GraphConstructionMode::BUFFER_SWEEP:
+            {
+                auto boundingBoxPolygonWrapper = [this](const SettlementShape<S> & settPolygon ){
+                    /* Create scaled aaBB containing at least all points reachable from the polygon within the maximum edge distance*/
+                    auto aaBB = fishnet::geometry::Rectangle<fishnet::math::DEFAULT_NUMERIC>(settPolygon);
+                    double distanceMetersTopLeftBotLeft = this->distanceFunction({aaBB.left(),aaBB.top()},{aaBB.left(),aaBB.bottom()});
+                    double scale = (this->config.distanceThreshold / distanceMetersTopLeftBotLeft) +1;
+                    return fishnet::geometry::BoundingBoxWrapper(settPolygon,aaBB.scale(scale));
+                };
+                fishnet::util::AllOfPredicate<S,S> neighbouringPredicate;
+                /* add all neighbouring predicates to composite predicate */
+                neighbouringPredicate.add(distancePredicate);
+                //std::ranges::for_each(config.initNeighbouringPredicates<S>(),[&neighbouringPredicate](const auto & predicate){neighbouringPredicate.add(predicate);});
+                auto shortCircuitPredicate = [neighbouringPredicate= std::move(neighbouringPredicate)](const fishnet::geometry::BoundingBoxWrapper<SettlementShape<S>> & lhs, const fishnet::geometry::BoundingBoxWrapper<SettlementShape<S>> & rhs){
+                    return lhs.getBoundingBox().overlap(rhs.getBoundingBox()) && neighbouringPredicate(lhs.getPolygon(),rhs.getPolygon());
+                };
+                const size_t MAX_NEIGHBORS_PER_NODE = config.args.at(GraphConstructionConfig::MAX_NEIGHBORS_KEY).get<size_t>();
+                return fishnet::geometry::PolygonNeighbours::sweepTemplate(this->settlements,shortCircuitPredicate,boundingBoxPolygonWrapper,MAX_NEIGHBORS_PER_NODE);
+            }
+            default:
+                throw std::invalid_argument("Unknown graph construction mode. Aborting.");
+        }
+    }
 
 public:
     GraphConstruction(const fishnet::Shapefile & primaryInput,
@@ -77,7 +121,7 @@ public:
         if(this->settlements.empty()){
             std::cerr << "Warning: No settlements read from primary input, returning empty graph" << std::endl;
         }else {
-            auto distanceFromBoundingBoxFilter = DistancePredicate(this->distanceFunction, fishnet::geometry::minimalBoundingBox(this->settlements), this->config.bufferDistanceMeters);
+            auto distanceFromBoundingBoxFilter = DistancePredicate(this->distanceFunction, fishnet::geometry::minimalBoundingBox(this->settlements), this->config.distanceThreshold);
             auto additionalSettlements = SettlementShape<S>::template read<fishnet::Shapefile>(secondaryInputs, reader, HashingFileReferenceMapper{}, distanceFromBoundingBoxFilter);
             this->settlements.insert(this->settlements.end(), additionalSettlements.begin(), additionalSettlements.end());
         }
@@ -92,21 +136,7 @@ public:
                 SettlementShapeDeserializer<S>{} // not used
             )
         );
-        auto boundingBoxPolygonWrapper = [this](const SettlementShape<S> & settPolygon ){
-            /* Create scaled aaBB containing at least all points reachable from the polygon within the maximum edge distance*/
-            auto aaBB = fishnet::geometry::Rectangle<fishnet::math::DEFAULT_NUMERIC>(settPolygon);
-            double distanceMetersTopLeftBotLeft = this->distanceFunction({aaBB.left(),aaBB.top()},{aaBB.left(),aaBB.bottom()});
-            double scale = (this->config.bufferDistanceMeters / distanceMetersTopLeftBotLeft) +1;
-            return fishnet::geometry::BoundingBoxWrapper(settPolygon,aaBB.scale(scale));
-        };
-        fishnet::util::AllOfPredicate<S,S> neighbouringPredicate;
-        /* add all neighbouring predicates to composite predicate */
-        neighbouringPredicate.add(DistanceBiPredicate(distanceFunction,config.bufferDistanceMeters));
-        //std::ranges::for_each(config.initNeighbouringPredicates<S>(),[&neighbouringPredicate](const auto & predicate){neighbouringPredicate.add(predicate);});
-        auto shortCircuitPredicate = [neighbouringPredicate= std::move(neighbouringPredicate)](const fishnet::geometry::BoundingBoxWrapper<SettlementShape<S>> & lhs, const fishnet::geometry::BoundingBoxWrapper<SettlementShape<S>> & rhs){
-            return lhs.getBoundingBox().overlap(rhs.getBoundingBox()) && neighbouringPredicate(lhs.getPolygon(),rhs.getPolygon());
-        };
-        auto result = fishnet::geometry::findNeighbouringPolygonsTemplate(this->settlements,shortCircuitPredicate,boundingBoxPolygonWrapper,config.maxNeighborsPerNode);
+        auto result = this->compute_neighbours();
         graph.addNodes(this->settlements);
         graph.addEdges(result);
     }
