c8e8813926fbdd169e620cff772721a6ba6a4a70
Lorenz Gruber <lorenz.gruber@uni-wuerzburg.de>
2026-08-20 17:04:59 +0200

    chore: header for constants within workflow

---
+++ src/AfricapolisConstants.hpp
@@ -0,0 +1,11 @@
+#pragma once
+#include <string>
+
+namespace Africapolis {
+
+static inline const std::string CLUSTER_ID_FIELD = "ClusterID";
+static inline const std::string FROM_ID_FIELD = "FromID";
+static inline const std::string TO_ID_FIELD = "ToID";
+static inline const size_t NOISE_CLUSTER_ID = 9999999999999;
+
+}
